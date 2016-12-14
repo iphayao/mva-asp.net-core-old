4f62cc64f59285bdb57dadf2272b142c15005beb
Phayao Boonon <phayaob@gmail.com>
2016-12-14 22:20:15 +0700

    add asp.net to say hello, world on the web

--- 01_helloworld/Program.cs
+++ 01_helloworld/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace ConsoleApplication
 {
+    public class Startup 
+    {
+        public void Configure(IApplicationBuilder app) 
+        {
+            app.Run(
+                context => { 
+                    return context.Response.WriteAsync("Hello, World on the web");
+                }
+            );
+        }
+    }
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var name = Console.ReadLine();
-            Console.WriteLine("Hello {0} welcome to C#", name);
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            
+            host.Run();
         }
     }
 }
