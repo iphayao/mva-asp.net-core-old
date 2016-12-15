2f2ccb45e82b2273d2006516a7a91973eb5addc4
Phayao Boonon <phayaob@gmail.com>
2016-12-15 22:29:56 +0700

    add configuration with json appsettings file.

--- WebApplication/src/WebApplication/Startup.cs
+++ WebApplication/src/WebApplication/Startup.cs
@@ -7,11 +7,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApplication
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                                .SetBasePath(env.ContentRootPath)
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -31,7 +42,7 @@ namespace WebApplication
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"Hello { env.EnvironmentName }!");
+                await context.Response.WriteAsync($"Hello { env.EnvironmentName } it's something {Configuration["message"]}");
             });
 
         }
