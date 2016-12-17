9062f9413cc38a02de2cfff6540e714046ee0a00
Phayao Boonon <phayaob@gmail.com>
2016-12-17 11:04:47 +0700

    add date formate for movie model

--- WebApplicationMovie/src/WebApplicationMovie/Models/Movie.cs
+++ WebApplicationMovie/src/WebApplicationMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace WebApplicationMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
