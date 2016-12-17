cbd341be960f67f031d7556084d4a7140f5c393d
Phayao Boonon <phayaob@gmail.com>
2016-12-17 10:15:07 +0700

    add movie model/controller and entity framework database

--- WebApplicationMovie/src/WebApplicationMovie/Data/Migrations/20161217031115_Initial.Designer.cs
+++ WebApplicationMovie/src/WebApplicationMovie/Data/Migrations/20161217031115_Initial.Designer.cs
@@ -8,9 +8,10 @@ using WebApplicationMovie.Data;
 namespace WebApplicationMovie.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161217031115_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
